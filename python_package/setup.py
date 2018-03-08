749b77a70b2fafa873950a80e78bdc28affbaae7
karansthr <karansthr97@gmail.com>
2018-03-08 16:45:20 +0530

    uploaded to pypi

--- python_package/setup.py
+++ python_package/setup.py
@@ -1,18 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name='url-shortener',
-    version='0.1dev',
-    packages=['shortener'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    name='urlshortener-cli',
+    version='0.4dev',
     long_description='A command-line tool for shortening long URLs',
     author="Karan Suthar",
     author_email="karansthr97@gmail.com",
+    license="MIT License",
+    url="https://github.com/karansthr/Url-Shortener",
+    packages=['shortener'],
     entry_points={
         'console_scripts': ['shrt=shortener:main'],
     },
     install_requires=[
         "requests==2.18.4",
-        "pyperclip"
+        "pyperclip==1.6.0",
     ],
+    keywords="url shortener",
 )
